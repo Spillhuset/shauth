fbe7c2e5cc91963e35d5d5b2957fdddcfbd89940
Glenn <glenn@not-tech.support>
2023-09-22 16:53:43 +0000

    fix(shinfo): callback
    Signed-off-by: Glenn <glenn@not-tech.support>

--- src/systems/shinfo.ts
+++ src/systems/shinfo.ts
@@ -2,6 +2,6 @@ import type { System } from ".";
 
 export default {
   name: "shinfo.but-it-actually.works",
-  callbackUri: "https://shinfo.but-it-actually.works/callback",
+  callbackUri: "https://shinfo.but-it-actually.works/auth",
   encryptionKey: SYSTEM_SHINFO_KEY,
 } satisfies System;
