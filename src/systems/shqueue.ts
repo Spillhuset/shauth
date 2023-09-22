108bd95d8097a7660961f7efc4d3fcf7fa7fc49c
Glenn <glenn@not-tech.support>
2023-09-22 16:18:37 +0000

    fix(shqueue): fix callback
    Signed-off-by: Glenn <glenn@not-tech.support>

--- src/systems/shqueue.ts
+++ src/systems/shqueue.ts
@@ -2,6 +2,6 @@ import type { System } from ".";
 
 export default {
   name: "shqueue.but-it-actually.works",
-  callbackUri: "https://shqueue.but-it-actually.works/callback",
+  callbackUri: "https://shqueue.but-it-actually.works/auth",
   encryptionKey: SYSTEM_SHQUEUE_KEY,
 } satisfies System;
